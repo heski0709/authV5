a3e0f333da134ee06683d916df0a1b922470a302
heski0709 <heski0709@gmail.com>
2024-02-11 19:32:39 +0900

    Login Page Components

---
+++ components/auth/login-form.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { CardWrapper } from "@/components/auth/card-wrapper"
+
+export const LoginForm = () => {
+    return (
+        <CardWrapper
+            headerLabel="Welcome back"
+            backButtonHref="/auth/register"
+            backButtonLabel="Don't have an account?"
+            showSocial
+        >
+            LoginForm
+        </CardWrapper>
+    )
+}
