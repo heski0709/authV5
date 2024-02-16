a970e92727a78b4825aa4f3ce2d038b95deadedf
heski0709 <heski0709@gmail.com>
2024-02-16 21:00:25 +0900

    feat: user login, logout

--- action/login.ts
+++ action/login.ts
@@ -1,15 +1,39 @@
-"use server"
-import * as z from "zod"
-import { revalidatePath } from "next/cache";
+"use server";
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { LoginSchema } from "@/schema";
+import { AuthError } from "next-auth";
+import * as z from "zod";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
     const validatedFields = LoginSchema.safeParse(values);
 
     if (!validatedFields.success) {
-        return { error: "Invalid fields!"};
+        return { error: "Invalid fields!" };
     }
 
-    return { success: "Email sent!"}
-    
-}
+    const { email, password } = validatedFields.data;
+
+    try {
+        await signIn("credentials", {
+            email,
+            password,
+            redirectTo: DEFAULT_LOGIN_REDIRECT,
+        });
+
+        return { success: ''}
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case "CredentialsSignin":
+                    return { error: "Invalid credentials!" };
+                default:
+                    console.log(error);
+                    
+                    return { error: "Something went wrong!" };
+            }
+        }
+
+        throw error
+    }
+};
