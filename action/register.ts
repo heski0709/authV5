6e06df6ca9a21dedb8f547d77662b12005f9e86f
heski0709 <heski0709@gmail.com>
2024-02-16 16:37:49 +0900

    feat: create user

--- action/register.ts
+++ action/register.ts
@@ -1,14 +1,36 @@
 "use server"
+import { getUserByEmail } from "@/data/user";
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schema";
+import bcrypt from "bcrypt";
 import * as z from "zod";
-
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
+
 
     if (!validatedFields.success) {
         return { error: "Invalid fields!"};
     }
 
-    return { success: "Email sent!"}
+    const { name, email, password } = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await getUserByEmail(email)
+
+    if (existingUser) {
+        return { error: "Email already in use!"}
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    })
+
+    // TODO: Send verification token email
+
+    return { success: "User created!"}
     
 }
